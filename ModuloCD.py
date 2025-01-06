1645187efa2dd2decb40a0364b054eb6938b38af
PatricioNav <patricio.navarrete@casagrande.edu.ec>
2025-01-05 22:32:56 -0500

    ModuloCD.py

--- ModuloCD.py
+++ ModuloCD.py
@@ -7,4 +7,11 @@ def generar_portada():
     st.markdown("<h2 style='color:blue;'>Autor: Patricio Navarrete</h2>", unsafe_allow_html=True)
     st.caption('Paradigmas de Programación para Inteligencia Artificial y Análisis de Datos C2024 P1')
     with st.expander("Ver detalles del Proyecto"):
-        st.write("Este proyecto se divide en dos partes: Aquí puedes añadir información técnica, métricas adicionales o gráficos secundarios.")
+        texto_largo = """
+        La aplicación creada en un ambiente visual atractivo para el usuario, permite a los usuarios cargar datasets, explorar, 
+        realizar EDA y visualizar resultados.
+        También permite aplicar a un dataset predefinido, métodos de regresión para la predicción de datos, donde se visualiza los 
+        principales hallazgos a través de las métricas de evaluación y también de manera gráfica para su mejor interpretación.  
+        Al final se puede exportar a formato PDF esos resultados.
+        """
+        st.write(texto_largo)
